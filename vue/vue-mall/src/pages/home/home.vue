767355ca61226a34c2eace1e6fe0fdf0bcb5dad1
cysgg <279709624@qq.com>
2019-09-26 18:44:59 +0800

    mall project

---
+++ vue/vue-mall/src/pages/home/home.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <header class="home-header wrap" :class="{'active' : headerActive}">
+      <router-link tag="i" to="./category" class="iconfont icon-caidan"></router-link>
+      <div class="header-search">
+        <span class="app-name">M</span>
+        <i class="iconfont icon-search"></i>
+        <router-link tag="span" class="search-title" to="./search">家电返场同价11.11</router-link>
+      </div>
+      <router-link tag="span" to="./login" v-if="!isLogin">登录</router-link>
+      <router-link tag="span" to="./user" v-else>
+          <i class="iconfont icon-iconyonghu"></i>
+      </router-link>
+    </header>
+    <nav-bar></nav-bar>
+    <slide />
+  </div>
+</template>
+
+<script>
+import navBar from '../../components/navBar'
+import header from '../../components/mHeader'
+import slide from '../../components/common/slide'
+export default {
+  name: 'home',
+  components: {
+    navBar,
+    slide,
+    header
+  },
+  data () {
+    return {
+      headList: [],
+      categoryList: [],
+      isLogin: false
+    }
+  },
+  beforeCreate () {
+    if (!this.isLogin) {
+      this.$route.redirect('/login')
+    }
+  },
+  created () {
+    // load data
+  },
+  mounted () {
+    window.addEventListener('scroll', () => {})
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
