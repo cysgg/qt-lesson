95020718a0b4ed49b2b9db4ebeb00d14faed3b4c
cysgg <279709624@qq.com>
2019-10-14 19:01:33 +0800

    koa 尝鲜

--- koa/koa-hello/index.js
+++ koa/koa-hello/index.js
@@ -1,9 +1,31 @@
 const Koa = require('koa')
+const KoaBody = require('koa-body')
+const path = require('path')
 const app = new Koa()
+
+app.use(KoaBody({
+  multipart: true,
+  formidable: {
+    maxFileSize: 200 * 1024 * 1024,
+    keepExtensions: true,
+    uploadDir: path.join(__dirname, '/static/images')
+  }
+}))
 
 app.use(async ctx => {
   const method = ctx.method
   const path = ctx.path
+  console.log(method, path);
+  if (method === 'GET' && path === '/') {
+    ctx.body = `
+    <form action="/upload" enctype="multipart/form-data" method="post">
+      <input type="file" name="avatar" id="">
+      <input type="submit" value="tijao">
+    </form>
+    `
+  } else if (method === 'POST' && path === '/upload') {
+    console.log(ctx.request.files)
+  }
 })
 
 app.listen(3001, () => {
--- koa/koa-hello/readme.md
+++ koa/koa-hello/readme.md
@@ -5,3 +5,12 @@
 提供数据：
   现在api -> json
 
+## ctx
+  ctx: {
+    req,
+    res
+  }
+
+  第三方中间件 都是对ctx 继续增强
+  koa-body
+  ctx.req.files
