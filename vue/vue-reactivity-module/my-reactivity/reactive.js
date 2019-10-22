968f7d5dc415e07f0708e496a7066156adf38abe
cysgg <279709624@qq.com>
2019-10-22 17:48:10 +0800

    vue observer

--- vue/vue-reactivity-module/my-reactivity/reactive.js
+++ vue/vue-reactivity-module/my-reactivity/reactive.js
@@ -1,11 +1,30 @@
-export function reactive (target) {
-  const observer = new Proxy (target, {
-    set: function () {
-      
+import { track, trigger } from './effect.js';
+var obj = { a: 1 }
+obj.a = 100;
+export function reactive(target) {
+  const obderved = new Proxy(target, {
+    set: (target, key, val, reactive) => {
+      // 恢复它 '赋值' 的一个行为
+      const result = Reflect.set(target, key, val, reactive)
+      trigger(target, key)
+      return result
     },
-    get: function () {
-      
+    get: (target, key, receiver) => {
+      // mobx 也有 effect 也有依赖收集
+      // 依赖收集
+      console.log('依赖收集');
+      const res = Reflect.get(target, key, receiver);
+      // {
+      //   origin: {
+      //     count: {
+      //       fn1,
+      //       fn2
+      //     }
+      //   }
+      // }
+      track(target, 'get', key);
+      return res;
     }
   });
-  return observer
+  return obderved;
 }
