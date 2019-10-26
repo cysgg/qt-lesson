e68b091dde80f41b93f80968cc55c9244ad47526
cysgg <279709624@qq.com>
2019-10-26 17:04:06 +0800

    爬数据

--- js/node/spider-spa/book.js
+++ js/node/spider-spa/book.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer')
 const $ = require('cheerio')
-const url = 'https://juejin.im/books'
+const url = 'https://www.dongqiudi.com/'
+const fs = require('fs')
 
 
 async function run() {
@@ -12,13 +13,38 @@ async function run() {
     await page.goto(url, {
         waitUntil: 'networkidle2'
     });
-    const html = await page.content()
-    const books =  $('.info',html)
-    books.each(function(){
-        const price = $('.price-text',this).text()
-        mysql.add(price)
-        console.log(price);
+    let html = await page.content()
+    let wrap = $('.home-wrap', html)
+    let List= []
+    console.log(wrap.find('.match-list').length);
+    
+    wrap.find('.match-list').each((i, match) => {
+        let date = $(match).find('.date').text().trim()
+        let matchList = []
+        $(match).find('.match-item').each((index, item) => {
+            let time =  $(item).find('.start-min').text()
+            let match_type = $(item).find('.round').text()
+            let team_a_name = $(item).find('.team-a .teama-name').text()
+            let team_a_img = $(item).find('.team-a img').attr('src')
+            let team_b_name = $(item).find('.team-b .teamb-name').text()
+            let team_b_img = $(item).find('.team-b img').attr('src')
+            let score_dom = $(item).find('.vs-score-wrap .score') ||  $(item).find('.vs-score-wrap .feature')
+            let score_or_VS = score_dom.text().trim() || 'VS'
+            matchList.push({time, match_type, team_a_name, team_a_img, team_b_name, team_b_img, score_or_VS})
+        })
+        console.log(matchList);
+        
+        List.push({date, matchList})
     })
+    // console.log(List);
+    
+    // console.log(JSON.stringify(List))
+    fs.writeFileSync('./mfwtext.js', JSON.stringify(List), { 'flag': 'a' })
+    // books.each(function(){
+    //     const price = $('.price-text',this).text()
+    //     mysql.add(price)
+    //     console.log(price);
+    // })
     // await page.waitFor(2000);
     // await page.type('#login_field', '123456')
     // await page.type('#password', '123456')
